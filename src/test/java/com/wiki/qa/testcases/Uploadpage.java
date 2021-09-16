2bf548a0fd63c3e0f944847f63399a53a515a26b
ashwinipawar12345 <ashwini.pawar48@gmail.com>
2021-09-16 23:33:52 +0530

    modifid uplod page file..

--- src/test/java/com/wiki/qa/testcases/Uploadpage.java
+++ src/test/java/com/wiki/qa/testcases/Uploadpage.java
@@ -1,2 +1,9 @@
 //This is new test cases
 //This test will check editing new cutomer..
+
+
+//This is code added newly..
+//This is code added newly..
+//This is code added newly..
+//This is code added newly..
+
